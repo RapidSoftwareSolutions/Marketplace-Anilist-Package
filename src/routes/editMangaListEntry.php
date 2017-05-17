8cc2b38a8a1edba685c22e209e92fc827c1ca16c
ivz-dev <zakutynsky@yahoo.com>
2017-05-17 14:23:31 +0300

    create block

---
+++ src/routes/editMangaListEntry.php
@@ -0,0 +1,82 @@
+<?php
+
+$app->post('/api/Anilist/editMangaListEntry', function ($request, $response) {
+
+    $settings = $this->settings;
+    $checkRequest = $this->validation;
+    $validateRes = $checkRequest->validate($request, ['accessToken','id','listStatus','notes','hidden']);
+
+    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+    } else {
+        $post_data = $validateRes;
+    }
+
+    $accessToken = $post_data['args']['accessToken'];
+    $userId = $post_data['args']['userId'];
+    $query_str = $settings['default_url'] . "animelist";
+    $client = $this->httpClient;
+
+    $data['id'] = $post_data['args']['id'];
+    $data['list_status'] = $post_data['args']['listStatus'];
+    $data['notes'] = $post_data['args']['notes'];
+    $data['hidden_default'] = $post_data['args']['hidden'];
+
+    try {
+        $resp = $client->put($query_str, [
+            'headers' => [
+                "Authorization" => "Bearer $accessToken",
+                "Content-Type" => "application/json"
+            ],
+            'json' => $data
+        ]);
+        $responseBody = $resp->getBody()->getContents();
+
+        if(in_array($resp->getStatusCode(), ['200', '201', '202', '203', '204'])) {
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+            if(empty($result['contextWrites']['to'])) {
+                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+            }
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = json_decode($responseBody);
+        }
+
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ServerException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ConnectException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'INTERNAL_PACKAGE_ERROR';
+        $result['contextWrites']['to']['status_msg'] = 'Something went wrong inside the package.';
+
+    }
+
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+
+});
